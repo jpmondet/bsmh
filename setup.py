9223f51f85a9c2373d1efe8a74dfcfeda133a6aa
jpmondet <jp@mondet.org>
2020-10-27 06:31:13 +0100

    Add all files needed for CI (and most importantly, windows build which I can't do on my machine)

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name='bsmh',
+      version='0.01',
+      description='bs maps handler',
+      author='jpmondet',
+      author_email='github.com/jpmondet',
+      url='github.com/jpmondet/bsmh',
+      packages=['bsmh'],
+      setup_requires=['wheel'],
+     )
